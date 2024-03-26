f90b5febbc25276357bdc5739cc122a07583f0fd
Menglin Zhao <116864187+Gigi77789@users.noreply.github.com>
2024-03-26 11:06:44 -0400

    Update howManyPlayersPage.xaml.cs

--- howManyPlayersPage.xaml.cs
+++ howManyPlayersPage.xaml.cs
@@ -11,8 +11,22 @@ namespace MAUICardsGUI
 
         private async void PlayerNumbersButton_Clicked(object sender, EventArgs e)
         {
-           int numberOfPlayers = int.Parse(NumberOfPlayers.Text); 
-           await Navigation.PushAsync(new NameOfPlayersPage(numberOfPlayers));
+            // Convert entered text to numbers
+            bool isNumber = int.TryParse(NumberOfPlayers.Text, out int numberOfPlayers);
+
+            //Check if the conversion to number is successful and the number is between 1 and 4
+            if (isNumber && numberOfPlayers >= 1 && numberOfPlayers <= 4)
+            {
+                // Hide error message if input is valid
+                ErrorMessageLabel.IsVisible = false;
+
+                // Navigate to a new page and pass the number of players
+                await Navigation.PushAsync(new NameOfPlayersPage(numberOfPlayers));
+            }
+            else
+            {
+                ErrorMessageLabel.IsVisible = true; 
+            }
         }
       
     }
