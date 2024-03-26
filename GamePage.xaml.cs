cd2133ad990743a97d70415bbc0091b1762ba96f
Menglin Zhao <116864187+Gigi77789@users.noreply.github.com>
2024-03-26 14:36:54 -0400

    Update GamePage.xaml.cs

--- GamePage.xaml.cs
+++ GamePage.xaml.cs
@@ -3,20 +3,22 @@ namespace MAUICardsGUI;
 public partial class GamePage : ContentPage
 {
     private Deck deck = new Deck();
+
     private Player player1 = new Player("Player#1");
     private Player player2 = new Player("Player#2");
     private Player player3 = new Player("Player#3");
     private Player player4 = new Player("Player#4");
+
     private Game game;
-    public PlayerStatus status = PlayerStatus.active;
+    //public PlayerStatus status = PlayerStatus.active;
 
     public GamePage(List<string> playerNames)
     {
         InitializeComponent();
         game = new Game(new CardTable());
-        game.AddPlayer("Player#1");
         deck.Shuffle();
 
+        var players = new List<Player> { player1, player2, player3, player4 };
         var playerLabels = new List<Label> { player1Name, player2Name, player3Name, player4Name };
         var playerLayouts = new List<VerticalStackLayout> { player1Layout, player2Layout, player3Layout, player4Layout };
 
@@ -26,64 +28,52 @@ public partial class GamePage : ContentPage
             layout.IsVisible = false;
         }
 
-        // shoe the content based on the number of the names and name itslef
-        for (int i = 0; i < playerLabels.Count; i++)
+        // Assign names to players and make the layouts visible
+        for (int i = 0; i < playerNames.Count; i++)
         {
-            if (i < playerNames.Count && !string.IsNullOrWhiteSpace(playerNames[i]))
+            if (!string.IsNullOrWhiteSpace(playerNames[i]))
             {
+                players[i].Name = playerNames[i]; // Assign the name from the input list to the player
                 playerLabels[i].Text = playerNames[i];
                 playerLayouts[i].IsVisible = true;
             }
         }
     }
 
-    private void Player1HitClicked(object sender, EventArgs e)
+    private void PlayerHitClicked(Player player, Label cardLabel, Label statusLabel, Image cardImage)
     {
-
-        if (player1.status == PlayerStatus.active)
+        /*This method accepts a player object as parameters along with the tags and images associated 
+      * with that player. Then, I modify each specific click event handler method so that they call
+      * this general method and pass the corresponding parameters to it*/
+        if (player.status == PlayerStatus.active)
         {
             var card = deck.DealTopCard();
-            player1.cards.Add(card);
-            game.ScoreHand(player1); // renew players score
-            UpdatePlayer1CardDisplay(player1);
-            player1CardImage.Source = ImageSource.FromFile(card.ImagePath); // get images
+            player.cards.Add(card);
+            game.ScoreHand(player);
+            UpdatePlayerCardDisplay(player, cardLabel, statusLabel);
+            cardImage.Source = ImageSource.FromFile(card.ImagePath);
+
         }
+    }
+
+    private void Player1HitClicked(object sender, EventArgs e)
+    {
+        PlayerHitClicked(player1, player1CardLabel, player1StatusLabel, player1CardImage);
     }
 
     private void Player2HitClicked(object sender, EventArgs e)
     {
-        if (player2.status == PlayerStatus.active)
-        {
-            var card = deck.DealTopCard();
-            player2.cards.Add(card);
-            game.ScoreHand(player2);
-            UpdatePlayer2CardDisplay(player2);
-            player2CardImage.Source = ImageSource.FromFile(card.ImagePath);
-        }
+        PlayerHitClicked(player2, player2CardLabel, player2StatusLabel, player2CardImage);
     }
 
     private void Player3HitClicked(object sender, EventArgs e)
     {
-        if (player3.status == PlayerStatus.active)
-        {
-            var card = deck.DealTopCard();
-            player3.cards.Add(card);
-            game.ScoreHand(player3);
-            UpdatePlayer3CardDisplay(player3);
-            player3CardImage.Source = ImageSource.FromFile(card.ImagePath);
-        }
+        PlayerHitClicked(player3, player3CardLabel, player3StatusLabel, player3CardImage);
     }
 
     private void Player4HitClicked(object sender, EventArgs e)
     {
-        if (player4.status == PlayerStatus.active)
-        {
-            var card = deck.DealTopCard();
-            player4.cards.Add(card);
-            game.ScoreHand(player4);
-            UpdatePlayer4CardDisplay(player4);
-            player4CardImage.Source = ImageSource.FromFile(card.ImagePath);
-        }
+        PlayerHitClicked(player4, player4CardLabel, player4StatusLabel, player4CardImage);
     }
 
     private void Player1StayClicked(object sender, EventArgs e)
@@ -104,141 +94,96 @@ public partial class GamePage : ContentPage
     }
 
 
-  
 
-    private void UpdatePlayer1CardDisplay(Player player1) //display the info aboout the player1
+
+
+    private async void UpdatePlayerCardDisplay(Player player, Label cardLabel, Label statusLabel)
     {
-        // check other players status
-        bool allOtherPlayersBust = (player4.status == PlayerStatus.bust) &&
-                                    (player2.status == PlayerStatus.bust) &&
-                                    (player3.status == PlayerStatus.bust);
-        // renew status
-        player1.UpdateScoreAndStatus();
+        // Renew player's status
+        player.UpdateScoreAndStatus();
 
-        player1CardLabel.Text = $"{string.Join(", ", player1.cards.Select(c => c.name))} = {player1.score} / 21";
-        // LINQ expression used to select (Select) the name of each card (c.name) from all the cards of player 1: https://learn.microsoft.com/en-us/dotnet/csharp/linq/,https://learn.microsoft.com/en-us/dotnet/csharp/linq/standard-query-operators/projection-operations
-        if (allOtherPlayersBust)
-        {
-            player1.status = PlayerStatus.win;
-            player1StatusLabel.Text = "Win!";
-        }
-    
-        else if (player1.status == PlayerStatus.bust)
-        {
-            player1StatusLabel.Text = "Bust!";
-        }
+        // Update card display
+        cardLabel.Text = $"{string.Join(", ", player.cards.Select(c => c.name))} = {player.score} / 21";
 
-        else if (player1.status == PlayerStatus.win)
+        int activePlayerCount = 0;
+        Player lastActivePlayer = null;
+        string message = "";
+
+        // Check how many players are not bust
+        foreach (var p in new[] { player1, player2, player3, player4 })
         {
-            player1StatusLabel.Text = "Win!";
+            if (p.status == PlayerStatus.active)
+            {
+                activePlayerCount++;
+                lastActivePlayer = p; // Keep track of the last active player
+            }
         }
 
+        // Update status label based on game state
+        if (activePlayerCount == 1 && lastActivePlayer.status != PlayerStatus.bust)
+        {
+            lastActivePlayer.status = PlayerStatus.win;
+            statusLabel.Text = "Win!";
+            message = $"{lastActivePlayer.Name} wins! Do you want to continue or exit?";
+        }
+        else if (player.status == PlayerStatus.bust)
+        {
+            statusLabel.Text = "Bust!";
+            if (activePlayerCount == 0) // All players are bust
+            {
+                message = "All players are bust. Do you want to continue or exit?";
+            }
+        }
+        else if (player.status == PlayerStatus.win)
+        {
+            statusLabel.Text = "Win!";
+            message = $"{player.Name} wins! Do you want to continue or exit?";
+        }
         else
         {
-            player1StatusLabel.Text = "";
+            statusLabel.Text = "";
+        }
+
+        // If a game-ending condition has been met, display an alert
+        if (!string.IsNullOrEmpty(message))
+        {
+            bool answer = await DisplayAlert("Game Over", message, "Continue", "Exit");
+            if (answer)
+            {
+                await Navigation.PushAsync(new howManyPlayersPage());
+            }
+            else
+            {
+                // Logic to exit the game: Close the application's current window
+                var currentWindow = Application.Current.Windows.FirstOrDefault();
+                if (currentWindow != null)
+                {
+                    Application.Current.CloseWindow(currentWindow);
+                }
+            }
         }
     }
-    private void UpdatePlayer2CardDisplay(Player player2)
+    private void UpdatePlayer1CardDisplay(Player player)
     {
-
-        bool allOtherPlayersBust = (player1.status == PlayerStatus.bust) &&
-                                    (player3.status == PlayerStatus.bust) &&
-                                    (player4.status == PlayerStatus.bust);
-
-
-        player2.UpdateScoreAndStatus();
-
-
-        player2CardLabel.Text = $"{string.Join(", ", player2.cards.Select(c => c.name))} = {player2.score} / 21";
-
-
-        if (allOtherPlayersBust)
-        {
-            player2.status = PlayerStatus.win;
-            player2StatusLabel.Text = "Win!";
-        }
-
-        else if
-         (player2.status == PlayerStatus.bust)
-        {
-            player2StatusLabel.Text = "Bust!";
-        }
-        else if (player2.status == PlayerStatus.win)
-        {
-            player2StatusLabel.Text = "Win!";
-        }
-
-        else
-        {
-            player2StatusLabel.Text = "";
-        }
+        UpdatePlayerCardDisplay(player, player1CardLabel, player1StatusLabel);
     }
 
-    private void UpdatePlayer3CardDisplay(Player player3)
+    private void UpdatePlayer2CardDisplay(Player player)
     {
-
-        bool allOtherPlayersBust = (player1.status == PlayerStatus.bust) &&
-                                    (player2.status == PlayerStatus.bust) &&
-                                    (player4.status == PlayerStatus.bust);
-
-        player3.UpdateScoreAndStatus();
-
-        player3CardLabel.Text = $"{string.Join(", ", player3.cards.Select(c => c.name))} = {player3.score} / 21";
-
-        if (allOtherPlayersBust)
-        {
-            player3.status = PlayerStatus.win;
-            player3StatusLabel.Text = "Win!";
-        }
-
-        else if (player3.status == PlayerStatus.bust)
-        {
-            player3StatusLabel.Text = "Bust!";
-        }
-
-        else if (player3.status == PlayerStatus.win)
-        {
-            player3StatusLabel.Text = "Win!";
-        }
-
-        else
-        {
-            player3StatusLabel.Text = "";
-        }
+        UpdatePlayerCardDisplay(player, player2CardLabel, player2StatusLabel);
     }
 
-    private void UpdatePlayer4CardDisplay(Player player4)
+    private void UpdatePlayer3CardDisplay(Player player)
     {
-
-        bool allOtherPlayersBust = (player1.status == PlayerStatus.bust) &&
-                                    (player2.status == PlayerStatus.bust) &&
-                                    (player3.status == PlayerStatus.bust);
-
-        player4.UpdateScoreAndStatus();
-
-        player4CardLabel.Text = $"{string.Join(", ", player4.cards.Select(c => c.name))} = {player4.score} / 21";
-
-        if (allOtherPlayersBust)
-        {
-            player4.status = PlayerStatus.win;
-            player4StatusLabel.Text = "Win!";
-        }
-
-        else if (player4.status == PlayerStatus.bust)
-        {
-            player4StatusLabel.Text = "Bust!";
-        } 
-
-        else if (player4.status == PlayerStatus.win)
-        {
-            player4StatusLabel.Text = "Win!";
-        }
-
-        else
-        {
-            player4StatusLabel.Text = "";
-        }
+        UpdatePlayerCardDisplay(player, player3CardLabel, player3StatusLabel);
     }
+
+    private void UpdatePlayer4CardDisplay(Player player)
+    {
+        UpdatePlayerCardDisplay(player, player4CardLabel, player4StatusLabel);
+    }
+
+
 
 
 }
